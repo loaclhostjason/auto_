137f606962af3ebffa8246d2b8e1d838f60ad865
zhangjie <951995314@qq.com>
2018-06-15 16:29:23 +0800

    更新密码

--- console/app/assets.py
+++ console/app/assets.py
@@ -16,6 +16,7 @@ common_css = Bundle(
 common_js = Bundle(
     'vendor/nprogress/nprogress.js',
     'vendor/toastr/toastr.min.js',
+    'js/update_password.js',
     'js/class.js',
     'js/app.js',
     filters='jsmin',
--- console/app/static/js/users/users.js
+++ console/app/static/js/users/users.js
@@ -97,29 +97,4 @@ $(document).ready(function () {
     });
 
     // update_password
-
-    let update_password_modal = $('#update_password_modal');
-    update_password_modal.on('hide.bs.modal', function () {
-        users.hide_modal($(this));
-    });
-    $('.update_password').click(function () {
-        users.show_modal(update_password_modal, $(this));
-        update_password_modal.find('.modal-title').text('更换【' + $(this).data('username') + '】密码');
-    });
-    let update_pwd_btn;
-    update_password_modal.on('show.bs.modal', function (event) {
-        update_pwd_btn = $(event.relatedTarget);
-    });
-    $('.submit_update_password').click(function () {
-        let user_id = update_pwd_btn.data('id');
-        let params = update_password_modal.find('form').serialize();
-        $.post('/users/edit/' + user_id + '?type=update_pwd', params, function (resp) {
-            if (resp.success) {
-                sessionStorage.setItem("success", resp.message);
-                window.location.reload();
-            } else {
-                toastr.error(resp.message);
-            }
-        })
-    });
 });
