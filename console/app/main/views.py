08f46544d88c7f232a01c9ce9cde399cbdf3cf2c
zhangjie <zhangjie@tcxa.com.cn>
2018-06-21 17:23:06 +0800

    add edit data

--- console/app/main/views.py
+++ console/app/main/views.py
@@ -10,6 +10,7 @@ from config import Config
 import os
 from datetime import datetime
 from .models import *
+from collections import defaultdict
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -18,6 +19,47 @@ from .models import *
 def projects():
     projects = Project.query.order_by(Project.id).all()
     return render_template('main/projects.html', projects=projects)
+
+
+def get_project_children(project_id):
+    result = list()
+    first_relation = ProjectRelation.query.filter_by(project_id=project_id, level=1).first()
+    d = {
+        'level_1': first_relation.name,
+    }
+
+    second_relation = ProjectRelation.query.filter_by(parent_id=first_relation.id, level=2).all()
+    if not second_relation:
+        return result
+
+    for v in second_relation:
+        d['level_2'] = v.name
+
+        third_relation = ProjectRelation.query.filter_by(parent_id=v.id, level=3).all()
+        if third_relation:
+            for th in third_relation:
+                d['level_3'] = th.name
+
+                forth_relation = ProjectRelation.query.filter_by(parent_id=th.id, level=4).all()
+                if forth_relation:
+                    for forth in forth_relation:
+                        # d['level_4'] = forth.name
+                        # print(forth.name)
+                        d.update({'level_4': forth.name})
+                        result.append(d.copy())
+
+    return result
+
+
+@main.route('/project/data/edit/<int:project_id>', methods=['GET', 'POST'])
+@login_required
+def edit_project_data(project_id):
+    project = Project.query.get_or_404(project_id)
+
+    result = get_project_children(project_id)
+    # max_len = max([len(v) for v in result.values()])
+    print(result)
+    return render_template('main/edit_project_data.html', project=project, result=result)
 
 
 @main.route('/project/create_edit', methods=['GET', 'POST'])
