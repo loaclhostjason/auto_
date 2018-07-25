ad335309fb8d4babfead038167b69bd22b1fac75
zhangjie <951995314@qq.com>
2018-07-25 23:08:39 +0800

    util.py

--- console/util.py
+++ console/util.py
@@ -55,7 +55,7 @@ def change_data(new_data_init):
                 else:
                     new_data += '|' + v + ')'
 
-    print(new_data)
+    # print(new_data)
     if hav_i:
         new_data = '!' + new_data + ''
     return new_data
