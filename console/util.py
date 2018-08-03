bb62244ee2863ce096cce9e50f64059e7413a673
zhangjie <951995314@qq.com>
2018-08-03 20:43:16 +0800

    fix bug util

--- console/util.py
+++ console/util.py
@@ -562,7 +562,9 @@ class ExportXml(object):
         with open(files_path, 'r') as f:
             for line in f:
                 if '<DefaultValue' in line:
-                    line = line[:20] + '"' + line[20:-16] + '"' + line[-16:-1] + '\r'
+                    line = line[:20] + '"' + line[20:-16] + '"' + line[-16:-1] + '\r\n'
+                else:
+                    line = line + '\r\n'
                 file_data += line
 
         with open(files_path, 'w', encoding='utf-8') as f:
@@ -578,7 +580,9 @@ class ExportXml(object):
         with open(files_path, 'r') as f:
             for line in f:
                 if '<DefaultValue' in line:
-                    line = line[:20] + '"' + line[20:-16] + '"' + line[-16:-1] + '\r'
+                    line = line[:20] + '"' + line[20:-16] + '"' + line[-16:-1] + '\r\n'
+                else:
+                    line = line + '\r\n'
                 file_data += line
 
         with open(files_path, 'w', encoding='utf-8') as f:
