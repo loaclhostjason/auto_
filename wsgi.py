8bd8d0d4c42e7c876fc2823fe71dcc6225343809
zhangjie <951995314@qq.com>
2018-08-21 22:04:33 +0800

    setup

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.append(basedir)
+# 添加virtualenv的模块目录到系统路径
+venv_path = os.path.join(basedir, 'venv\Lib\site-packages')
+sys.path.append(venv_path)
+
+from app import app as application
