49ebae0b5eab77d42620c31ecea6440341d117f2
zhangjie <951995314@qq.com>
2018-08-22 22:52:32 +0800

    跨字段处理

--- console/app/manage/api.py
+++ console/app/manage/api.py
@@ -55,24 +55,24 @@ def add_attr_content():
     update_project_config_name(form_data, project_id)
 
     # update project data len
-    p_data = ProjectData.query.filter_by(project_id=project_id).all()
+    # p_data = ProjectData.query.filter_by(project_id=project_id).all()
 
-    bit_line, start_bit, byte_info = AttrContent.get_attr_info(project_relation_id, is_parent=True)
+    # bit_line, start_bit, byte_info = AttrContent.get_attr_info(project_relation_id, is_parent=True)
 
-    dif_did_len = math.ceil((bit_line + start_bit) / 8)
-    __byte_ = []
-    if dif_did_len == 1 or dif_did_len == 0:
-        __byte_.append('byte{}'.format(byte_info))
-    else:
-        for i in range(dif_did_len):
-            __byte_.append('byte{}'.format(i))
-
-    if p_data:
-        for nf in p_data:
-            contents = json.loads(nf.content or '{}')
-            _content = {k: v for k, v in contents.items() if k in __byte_}
-            nf.content = json.dumps(_content)
-            db.session.add(nf)
+    # dif_did_len = math.ceil((bit_line + start_bit) / 8)
+    # __byte_ = []
+    # if dif_did_len == 1 or dif_did_len == 0:
+    #     __byte_.append('byte{}'.format(byte_info))
+    # else:
+    #     for i in range(dif_did_len):
+    #         __byte_.append('byte{}'.format(i))
+    #
+    # if p_data:
+    #     for nf in p_data:
+    #         contents = json.loads(nf.content or '{}')
+    #         _content = {k: v for k, v in contents.items() if k in __byte_}
+            # nf.content = json.dumps(_content)
+            # db.session.add(nf)
 
     return jsonify({'success': True, 'message': '更新成功'})
 
